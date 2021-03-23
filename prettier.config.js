2e047b50599894510f2625cd1eee6d4283063893
Kirill Mager <kiril.maher@itechart-group.com>
2021-03-23 12:47:30 +0300

    feat: add prettier config file

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  endOfLine: 'lf',
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  printWidth: 100,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+};
