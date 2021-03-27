ea25fc1157555cc20f82fe04fdd56f871dfd7405
Anastasiya Sidarovich <anastasiya.sidarovich@digitech7.by>
2021-03-27 19:26:13 +0300

    feat: update menu list

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { MuiThemeProvider, createMuiTheme, createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import { BrowserRouter as Router, Route, Switch, Link as RouterLink } from 'react-router-dom';
 import { Main } from './components/Main';
 import { Login } from './components/Login';
@@ -17,13 +17,19 @@ import {
   List,
   ListItem,
   ListItemIcon,
+  ListItemText,
+  Collapse,
 } from '@material-ui/core';
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+
 import MenuIcon from '@material-ui/icons/Menu';
 import PersonIcon from '@material-ui/icons/Person';
 import HomeIcon from '@material-ui/icons/Home';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import GroupIcon from '@material-ui/icons/Group';
+import SportsEsportsIcon from '@material-ui/icons/SportsEsports';
+import ExpandLess from '@material-ui/icons/ExpandLess';
+import ExpandMore from '@material-ui/icons/ExpandMore';
+import StarBorder from '@material-ui/icons/StarBorder';
 
 import './App.css';
 
@@ -33,7 +39,8 @@ const useStyles = makeStyles((theme: Theme) =>
       flexGrow: 1,
     },
     appBar: {
-      boxShadow: '0px 2px 4px -1px rgb(42 68 78 / 20%), 0px 4px 5px 0px rgb(42 68 78 / 14%), 0px 1px 10px 0px rgb(42 68 78 / 12%)',
+      boxShadow:
+        '0px 2px 4px -1px rgb(42 68 78 / 20%), 0px 4px 5px 0px rgb(42 68 78 / 14%), 0px 1px 10px 0px rgb(42 68 78 / 12%)',
     },
     menuButton: {
       marginRight: theme.spacing(2),
@@ -45,6 +52,9 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     list: {
       width: 250,
+    },
+    nested: {
+      paddingLeft: theme.spacing(4),
     },
   }),
 );
@@ -60,8 +70,18 @@ const theme = createMuiTheme({
   },
   typography: {
     fontFamily: [
-      'Philosopher', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu',
-      'Cantarell', 'Fira Sans', '"Droid Sans"', '"Helvetica Neue"', 'sans-serif'
+      'Philosopher',
+      '-apple-system',
+      'BlinkMacSystemFont',
+      'Segoe UI',
+      'Roboto',
+      'Oxygen',
+      'Ubuntu',
+      'Cantarell',
+      'Fira Sans',
+      '"Droid Sans"',
+      '"Helvetica Neue"',
+      'sans-serif',
     ].join(','),
     allVariants: {
       color: '#2A444E',
@@ -73,6 +93,11 @@ const theme = createMuiTheme({
 const App = () => {
   const classes = useStyles();
   const [shown, setShown] = useState(false);
+  const [open, setOpen] = React.useState(true);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
 
   return (
     <Router>
@@ -92,7 +117,7 @@ const App = () => {
               <RouterLink to="/" style={{ textDecoration: 'none', color: 'white' }}>
                 <Typography variant="h6" className={classes.title}>
                   Lang
-              </Typography>
+                </Typography>
               </RouterLink>
             </Toolbar>
           </AppBar>
@@ -106,12 +131,12 @@ const App = () => {
                   variant="body1"
                   style={{ textDecoration: 'none' }}
                 >
-                  <ListItem button onClick={() => { }}>
+                  <ListItem button onClick={() => {}}>
                     <ListItemIcon>
-                      <HomeIcon color="primary"/>
+                      <HomeIcon color="primary" />
                     </ListItemIcon>
-                  Home
-                </ListItem>
+                    Домашняя
+                  </ListItem>
                 </Link>
 
                 <Link
@@ -121,12 +146,12 @@ const App = () => {
                   variant="body1"
                   style={{ textDecoration: 'none' }}
                 >
-                  <ListItem button onClick={() => { }}>
+                  <ListItem button onClick={() => {}}>
                     <ListItemIcon>
-                      <GroupIcon color="primary"/>
+                      <GroupIcon color="primary" />
                     </ListItemIcon>
-                  Our Team
-                </ListItem>
+                    Наша Команда
+                  </ListItem>
                 </Link>
 
                 <Link
@@ -136,12 +161,12 @@ const App = () => {
                   variant="body1"
                   style={{ textDecoration: 'none' }}
                 >
-                  <ListItem button onClick={() => { }}>
+                  <ListItem button onClick={() => {}}>
                     <ListItemIcon>
-                      <ExitToAppIcon color="primary"/>
+                      <ExitToAppIcon color="primary" />
                     </ListItemIcon>
-                  Login
-                </ListItem>
+                    Войти
+                  </ListItem>
                 </Link>
 
                 <Divider />
@@ -153,13 +178,81 @@ const App = () => {
                   variant="body1"
                   style={{ textDecoration: 'none' }}
                 >
-                  <ListItem button onClick={() => { }}>
+                  <ListItem button onClick={() => {}}>
                     <ListItemIcon>
-                      <PersonIcon color="primary"/>
+                      <PersonIcon color="primary" />
                     </ListItemIcon>
-                  Profile
-                </ListItem>
+                    Профиль
+                  </ListItem>
                 </Link>
+                <ListItem button onClick={handleClick}>
+                  <ListItemIcon>
+                    <SportsEsportsIcon color="primary" />
+                  </ListItemIcon>
+                  <ListItemText primary="Мини-игры" />
+                  {open ? <ExpandLess /> : <ExpandMore />}
+                </ListItem>
+                <Collapse in={open} timeout="auto" unmountOnExit>
+                  <List component="div" disablePadding>
+                    <Link
+                      component={RouterLink}
+                      to="/savannah"
+                      color="primary"
+                      variant="body1"
+                      style={{ textDecoration: 'none' }}
+                    >
+                      <ListItem button className={classes.nested}>
+                        <ListItemIcon>
+                          <StarBorder style={{ fill: '#dc622f' }} />
+                        </ListItemIcon>
+                        <ListItemText primary="Саванна" />
+                      </ListItem>
+                    </Link>
+                    <Link
+                      component={RouterLink}
+                      to="/speakit"
+                      color="primary"
+                      variant="body1"
+                      style={{ textDecoration: 'none' }}
+                    >
+                      <ListItem button className={classes.nested}>
+                        <ListItemIcon>
+                          <StarBorder style={{ fill: '#A632C6' }} />
+                        </ListItemIcon>
+                        <ListItemText primary="Скажи это" />
+                      </ListItem>
+                    </Link>
+                    <Link
+                      component={RouterLink}
+                      to="/audiochallendge"
+                      color="primary"
+                      variant="body1"
+                      style={{ textDecoration: 'none' }}
+                    >
+                      <ListItem button className={classes.nested}>
+                        <ListItemIcon>
+                          <StarBorder style={{ fill: '#1BD9DE' }} />
+                        </ListItemIcon>
+
+                        <ListItemText primary="Аудиовызов" />
+                      </ListItem>
+                    </Link>
+                    <Link
+                      component={RouterLink}
+                      to="/sprint"
+                      color="primary"
+                      variant="body1"
+                      style={{ textDecoration: 'none' }}
+                    >
+                      <ListItem button className={classes.nested}>
+                        <ListItemIcon>
+                          <StarBorder style={{ fill: '#32A2E5' }} />
+                        </ListItemIcon>
+                        <ListItemText primary="Спринт" />
+                      </ListItem>
+                    </Link>
+                  </List>
+                </Collapse>
               </List>
             </div>
           </Drawer>
