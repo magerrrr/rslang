83f407ec7576697138519a300191b450ad91040d
Kirill Mager <hi.kirill.m@gmail.com>
2021-03-28 13:31:06 +0300

    feat(SignIn Component): add ability to sign in (without design)

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,7 +1,36 @@
 import * as React from 'react';
-
+import { useHistory } from 'react-router-dom';
+import api from '../api';
 type Props = {};
 
 export const SignIn = (props: Props) => {
-  return <h1>Login</h1>;
+  const history = useHistory();
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  const handleSignIn = async () => {
+    try {
+      await api.auth.signIn({ email, password });
+      history.push('/');
+    } catch (e) {
+      return e;
+    }
+  };
+
+  return (
+    <>
+      <h1>Sign In</h1>
+      <label>
+        <input type="text" value={email} onChange={(event) => setEmail(event.target.value)} />
+      </label>
+      <label>
+        <input
+          type="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+      </label>
+      <button onClick={handleSignIn}>Sign In</button>
+    </>
+  );
 };
