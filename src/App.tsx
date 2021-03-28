81b670a98e8cc3f4c0f622192d0a61adbe8e287d
Kirill Mager <hi.kirill.m@gmail.com>
2021-03-28 17:45:20 +0300

    feat(Router): wrap profile component by PrivateRoute HOC for hide this component when user is not authenticated

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { PrivateRoute } from './components/PrivateRoute';
 import { Main } from './components/Main';
 import { SignUp } from './components/SignUp';
 import { SignIn } from './components/SignIn';
@@ -20,8 +21,8 @@ const App = () => {
       <MuiThemeProvider theme={theme}>
         <Menu />
         <Switch>
+          <PrivateRoute path={PROFILE.route} component={Profile} />
           <Route exact path={MAIN.route} component={Main} />
-          <Route path={PROFILE.route} component={Profile} />
           <Route path={TEAM.route} component={OurTeam} />
           <Route path={SIGN_IN.route} component={SignIn} />
           <Route path={SIGN_UP.route} component={SignUp} />
