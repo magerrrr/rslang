625d4499526397effe56a4d4c0c55b399f717949
Kirill Mager <hi.kirill.m@gmail.com>
2021-03-28 17:42:50 +0300

    feat(Profile Component): add ability to redirect to main page after sigh out

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,13 +1,17 @@
 import * as React from 'react';
+import { useHistory } from 'react-router-dom';
+import { ROUTES } from '../shared/constants';
 
 type Props = {};
 
 export const Profile = (props: Props) => {
+  const history = useHistory();
   const handleSignOut = () => {
     localStorage.removeItem('token');
     localStorage.removeItem('refreshToken');
     localStorage.removeItem('userId');
     localStorage.removeItem('name');
+    history.push(ROUTES.MAIN.route);
   };
 
   return (
