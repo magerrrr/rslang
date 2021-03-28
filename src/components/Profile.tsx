0beb0e64226c0fd0ffde0c7dfc7c76a75ad3b3b2
Kirill Mager <hi.kirill.m@gmail.com>
2021-03-28 23:21:54 +0300

    feat(Profile): get full info about user

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,11 +1,17 @@
 import * as React from 'react';
 import { useHistory } from 'react-router-dom';
 import { ROUTES } from '../shared/constants';
+import { baseURL } from '../api/urls';
+import useGetCurrentUserId from '../hooks/useGetCurrentUserId';
+import api from '../api';
 
 type Props = {};
 
 export const Profile = (props: Props) => {
   const history = useHistory();
+  const currentUserId = useGetCurrentUserId();
+  const { user } = api.users.getUser(currentUserId);
+
   const handleSignOut = () => {
     localStorage.removeItem('token');
     localStorage.removeItem('refreshToken');
@@ -17,6 +23,23 @@ export const Profile = (props: Props) => {
   return (
     <>
       <h1>Profile</h1>
+      {user && (
+        <>
+          {user.avatar && <img src={`${baseURL}/${user.avatar}`} alt="User Photo" />}
+          <p>
+            <span>Имя:</span>
+            <span>{user.name}</span>
+          </p>
+          <p>
+            <span>Email:</span>
+            <span>{user.email}</span>
+          </p>
+          <p>
+            <span>Id пользователя:</span>
+            <span>{user.id}</span>
+          </p>
+        </>
+      )}
       <button onClick={handleSignOut}>Выйти из профиля</button>
     </>
   );
