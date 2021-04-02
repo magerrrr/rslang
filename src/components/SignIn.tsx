66adc989670549a050b0d48ad2983db937477d9b
Sergey Naumenko <sergeynau@gmail.com>
2021-04-03 00:59:07 +0300

    feat: sign-in ui

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,12 +1,24 @@
 import * as React from 'react';
-import { useHistory } from 'react-router-dom';
+import { Link as RouterLink, useHistory } from 'react-router-dom';
+import { Link } from '@material-ui/core';
+import { ROUTES } from '../shared/constants';
+
 import api from '../api';
+import {
+  FormContainer,
+  StyledForm,
+  StyledInput,
+  StyledButton,
+} from './SignCommon';
+
 type Props = {};
 
 export const SignIn = (props: Props) => {
   const history = useHistory();
   const [email, setEmail] = React.useState('');
   const [password, setPassword] = React.useState('');
+
+  const { SIGN_UP } = ROUTES;
 
   const handleSignIn = async () => {
     try {
@@ -18,19 +30,41 @@ export const SignIn = (props: Props) => {
   };
 
   return (
-    <>
-      <h1>Sign In</h1>
-      <label>
-        <input type="text" value={email} onChange={(event) => setEmail(event.target.value)} />
-      </label>
-      <label>
-        <input
-          type="password"
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-        />
-      </label>
-      <button onClick={handleSignIn}>Sign In</button>
-    </>
+    <div
+      style={{
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <FormContainer>
+        <StyledForm autoComplete="off">
+          <label>
+            <StyledInput 
+              type="text" 
+              value={email} 
+              onChange={(event) => setEmail(event.target.value)} 
+            />
+          </label>
+          <label>
+            <StyledInput
+              type="password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+            />
+          </label>
+          <StyledButton onClick={handleSignIn}>Войти</StyledButton>
+          <Link
+              component={RouterLink}
+              to={SIGN_UP.route}
+              color="primary"
+              variant="body1"
+              style={{ textDecoration: 'none' }}
+          >Зарегистрироваться</Link>  
+        </StyledForm>
+      </FormContainer>
+    </div>
   );
 };
--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,9 +1,13 @@
 import * as React from 'react';
 import api from '../api';
 import { useHistory } from 'react-router-dom';
-import styled from 'styled-components';
 import ImageUploader from 'react-images-upload';
-import { Button, withStyles } from '@material-ui/core';
+import {
+  FormContainer,
+  StyledForm,
+  StyledInput,
+  StyledButton,
+} from './SignCommon';
 
 type Props = {};
 
@@ -107,47 +111,3 @@ export const SignUp = (props: Props) => {
     </div>
   );
 };
-
-const FormContainer = styled.div`
-  display: flex;
-  box-sizing: border-box;
-  width: 300px;
-  background-color: #f4f3fa;
-  padding: 30px;
-  margin-top: 30px;
-  border-radius: 20px;
-`;
-
-const StyledForm = styled.form`
-  width: 100%;
-`;
-
-const StyledInput = styled.input`
-  width: 100%;
-  box-sizing: border-box;
-  padding: 5px 10px;
-  border: 1px solid #c1b3f1;
-  border-radius: 5px;
-  margin-bottom: 20px;
-
-  &:focus {
-    outline-color: #a196ca;
-  }
-`;
-
-const StyledButton = withStyles({
-  root: {
-    background: 'linear-gradient(45deg, #9a8fb8 30%, #c1b3f1 90%)',
-    borderRadius: 5,
-    border: 0,
-    color: 'white',
-    height: 36,
-    padding: '0 30px',
-    boxShadow: '0 3px 5px 2px rgb(136 121 148 / 30%)',
-    width: '100%',
-    marginTop: 10,
-  },
-  label: {
-    textTransform: 'uppercase',
-  },
-})(Button);
