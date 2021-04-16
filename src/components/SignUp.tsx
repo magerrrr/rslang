9a698aabd706bbeddef0b2fac912349edb7f8d4c
Kirill Mager <kiril.maher@itechart-group.com>
2021-04-16 21:31:57 +0300

    feat(SignUp): add ability control sign Up form via useSignUp hook + styles

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,94 +1,80 @@
 import * as React from 'react';
-import api from '../api';
-import { useHistory } from 'react-router-dom';
+import useSignUpForm from '../hooks/useSignUpForm';
 import ImageUploader from 'react-images-upload';
 import styled from 'styled-components';
 import { Button, withStyles } from '@material-ui/core';
 
-type Props = {};
+type InputPropsType = { isError?: boolean };
 
-interface ISignUpData {
-  name: string;
-  email: string;
-  password: string;
-}
-
-const initialSignUpData = {
-  name: '',
-  email: '',
-  password: '',
-};
-
-export const SignUp = (props: Props) => {
-  const [signUpData, setSignUpData] = React.useState<ISignUpData>(initialSignUpData);
-  const [avatar, setAvatar] = React.useState<any>(null);
-  const history = useHistory();
-
-  const handleSubmitSignUp = async (e: any) => {
-    const formData = new FormData();
-    formData.append('name', signUpData.name);
-    formData.append('email', signUpData.email);
-    formData.append('password', signUpData.password);
-    formData.append('avatar', avatar, avatar.name);
-
-    await api.users.createNewUser(formData);
-    history.push('/');
-  };
-
-  const inputChangeHandler = (e: any) => {
-    setSignUpData((prevState) => ({
-      ...prevState,
-      [e.target.name]: e.target.value,
-    }));
-  };
-
-  const onDrop = (picture: any) => {
-    setAvatar(picture[0]);
-  };
+export const SignUp: React.FC = () => {
+  const {
+    placeholders,
+    names,
+    values,
+    handleChange,
+    handleBlur,
+    handleDrop,
+    setFieldAvatarToDefault,
+    isPasswordError,
+    isEmailError,
+    isNameError,
+    isAbleToSubmit,
+    errors,
+    submitForm,
+  } = useSignUpForm();
 
   return (
     <Wrapper>
       <h1>Регистрация</h1>
       <FormContainer>
-        <StyledForm autoComplete="off">
+        <StyledForm autoComplete="off" onSubmit={submitForm}>
           <StyledLabel>
             <StyledInput
               type="text"
-              name="name"
-              placeholder="Имя"
-              value={signUpData.name}
-              onChange={inputChangeHandler}
+              placeholder={placeholders.name}
+              name={names.name}
+              value={values.name}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              isError={isNameError}
             />
           </StyledLabel>
+          {isNameError && <StyledError>{errors.name}</StyledError>}
 
           <StyledLabel>
             <StyledInput
               type="text"
-              name="email"
-              placeholder="Е-мейл"
-              value={signUpData.email}
-              onChange={inputChangeHandler}
+              placeholder={placeholders.email}
+              name={names.email}
+              value={values.email}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              isError={isEmailError}
             />
           </StyledLabel>
+          {isEmailError && <StyledError>{errors.email}</StyledError>}
 
           <StyledLabel>
             <StyledInput
               type="password"
-              name="password"
-              placeholder="Пароль"
-              value={signUpData.password}
-              onChange={inputChangeHandler}
+              placeholder={placeholders.password}
+              name={names.password}
+              value={values.password}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              isError={isPasswordError}
             />
           </StyledLabel>
+          {isPasswordError && <StyledError>{errors.password}</StyledError>}
 
-          {avatar ? (
+          {values.avatar ? (
             <ImageSelected>
               <span>Аватар выбран</span>
-              <DeleteButton onClick={() => setAvatar(null)}>Удалить</DeleteButton>
+              <DeleteButton onClick={setFieldAvatarToDefault}>Удалить</DeleteButton>
             </ImageSelected>
           ) : (
             <ImageUploader
-              onChange={onDrop}
+              onChange={handleDrop}
               withIcon={true}
               singleImage={true}
               label="Максимальный размер фото 4мб"
@@ -101,7 +87,15 @@ export const SignUp = (props: Props) => {
             />
           )}
 
-          <StyledButton onClick={handleSubmitSignUp}>Регистрация</StyledButton>
+          <StyledButton type="submit" disabled={!isAbleToSubmit}>
+            Регистрация
+          </StyledButton>
+
+          {errors.serverResponseError && (
+            <StyledServerErrorContainer>
+              <StyledServerError>{errors.serverResponseError}</StyledServerError>
+            </StyledServerErrorContainer>
+          )}
         </StyledForm>
       </FormContainer>
     </Wrapper>
@@ -145,19 +139,73 @@ const StyledForm = styled.form`
 
 const StyledLabel = styled.label`
   width: 100%;
+  margin: 0;
 `;
 
 const StyledInput = styled.input`
   width: 100%;
   box-sizing: border-box;
   padding: 5px 10px;
-  border: 1px solid #c1b3f1;
+  border: ${({ isError }: InputPropsType) => `1px solid ${isError ? '#EC407A' : '#c1b3f1'}`};
+  margin-bottom: ${({ isError }: InputPropsType) => (isError ? '4px' : '22px')};
   border-radius: 5px;
-  margin-bottom: 20px;
 
   &:focus {
-    outline-color: #a196ca;
+    outline: none;
   }
+
+  animation: ${({ isError }: InputPropsType) => (isError ? 'wiggle 0.3s linear 1 normal' : 'none')};
+
+  @keyframes wiggle {
+    0% {
+      top: 0;
+      left: 0;
+      transform: rotate(0deg);
+    }
+    25% {
+      top: 1px;
+      left: 2px;
+      transform: rotate(-2deg);
+    }
+    50% {
+      top: 2px;
+      left: 0;
+      transform: rotate(0deg);
+    }
+    75% {
+      top: 1px;
+      left: -2px;
+      transform: rotate(2deg);
+    }
+    100% {
+      top: 0;
+      left: 0;
+      transform: rotate(0deg);
+    }
+  }
+`;
+
+const StyledError = styled.strong`
+  display: flex;
+  color: #ec407a;
+  min-height: 18px;
+  font-size: 12px;
+  margin-left: 5px;
+`;
+
+const StyledServerError = styled.strong`
+  display: flex;
+  color: #ec407a;
+  min-height: 18px;
+  font-size: 12px;
+  margin-left: 5px;
+  text-align: center;
+`;
+
+const StyledServerErrorContainer = styled.div`
+  margin-top: 15px;
+  display: flex;
+  justify-content: center;
 `;
 
 const DeleteButton = withStyles({
@@ -185,6 +233,13 @@ const StyledButton = withStyles({
     boxShadow: '0 3px 5px 2px rgb(136 121 148 / 30%)',
     width: '100%',
     marginTop: 10,
+    transition: 'all 0.4s',
+
+    '&:disabled': {
+      background: 'linear-gradient(45deg, #d4cfe4 30%, #d0c6f1 90%)',
+      boxShadow: 'none',
+      color: '#807575',
+    },
   },
   label: {
     textTransform: 'uppercase',
