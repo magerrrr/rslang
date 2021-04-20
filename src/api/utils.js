91a997e2b5fe319a2c914970be7424f80bc29011
Anastasiya Sidarovich <anastasiya.sidarovich@digitech7.by>
2021-04-20 10:58:40 +0300

    refactor(api): move filter logiic to utils

--- src/api/utils.js
+++ src/api/utils.js
@@ -16,3 +16,20 @@ const fetcher = async (url, token) => {
 };
 
 export default fetcher;
+
+export const aggregatedWordsFilter = (page, group, difficulty?) => {
+  let difficultyConstraint;
+  if (Array.isArray(difficulty)) {
+      const difficultyArr = [];
+      difficulty.map((item: any) => {
+        difficultyArr.push(`{"userWord.difficulty":"${item}"}`);
+        return item;
+      });
+     difficultyConstraint = `{"$or":[${difficultyArr.join(",")}]},`;
+  } else {
+     difficultyConstraint = difficulty ? `{"userWord.difficulty":"${difficulty}"},` : '';
+  }
+
+  return encodeURI(`{"$and":[${difficultyConstraint} {"page": ${page}}, {"group": ${group}}]}`);
+}
+
